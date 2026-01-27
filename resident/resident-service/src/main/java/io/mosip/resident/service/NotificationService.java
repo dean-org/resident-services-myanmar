b5226d6ce30e4b0a55d2558da33829cf22a3ca3a
Dharanidharan-S-04 <dean12345.dd@gmail.com>
2026-01-27 17:35:13 +0530

    Update NotificationService.java
    Signed-off-by: Dharanidharan-S-04 <dean12345.dd@gmail.com>

--- resident/resident-service/src/main/java/io/mosip/resident/service/NotificationService.java
+++ resident/resident-service/src/main/java/io/mosip/resident/service/NotificationService.java
@@ -111,7 +111,7 @@ public class NotificationService {
 	private static final String SEPARATOR = "/";
 	
 	public NotificationResponseDTO sendNotification(NotificationRequestDto dto) throws ResidentServiceCheckedException {
-		return sendNotification(dto, List.of("email"),dto.getEmail(), null);
+		return sendNotification(dto, List.of("email"),null, null);
 	}
 
 	public NotificationResponseDTO sendNotification(NotificationRequestDto dto, List<String> channels, String email, String phone) throws ResidentServiceCheckedException {
